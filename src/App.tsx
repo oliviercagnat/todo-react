44333bc7600a95857e11b854921a26ad71fce5e0
oliviercagnat <oliviercagnat@gmail.com>
2022-10-27 11:41:33 +0200

    modify prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {FC, ReactElement} from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
+import { DashBoard } from './pages/dashboard/dashboard';
 
 const App: FC = (): ReactElement => {
   return(
   <ThemeProvider theme={customTheme}>
     <CssBaseline />
-    <h1>Hello World</h1>
+    <DashBoard />
   </ThemeProvider>
   );
 }
